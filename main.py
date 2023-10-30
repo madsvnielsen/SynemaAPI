23f9b0832bbb4bc2adef6d213c479b426df219eb
madsvnielsen <mads@hvn.dk>
2023-10-30 14:26:46 +0100

    - set up venv - fixed more of the API

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
 from typing import Union
-
+import requests
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+API_URL = os.environ["APIURL"]
+
+headers = {
+    "Authorization": os.environ["APIKEY"],
+    "accept": "application/json"
+
+}
+
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    url = API_URL + "find/tt0816692?external_source=imdb_id"
+    response = requests.get(url, headers=headers)
+    return response.json()
 
 
 @app.get("/test")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"test": noob, "q": q}
+    return {"test": "noob", "q": q}
+
+@app.post("/user/login")
+def user_login():
+    print("Requested!!")
+    return {
+        "profile" : {
+            "id" : "apitest",
+            "name" : "Api works",
+            "email": "api@net.dk"
+        }
+    }
