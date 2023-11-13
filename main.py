2624b57408607e5e34e7d5fa198bf6fd97737fe8
MathiasW <s224293@student.dtu.dk>
2023-11-13 11:25:15 +0100

    Added firebase to the server and encrypted the api key

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+from http.client import HTTPException
 from typing import Union
 import requests
+import uuid
+
+from cryptography.fernet import Fernet
 from fastapi import FastAPI
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+key = os.environ["DecryptKey"]
+# using the key
+fernet = Fernet(key)
+
+# opening the encrypted file
+with open('synema.key', 'rb') as enc_file:
+    encrypted = enc_file.read()
+
+# decrypting the file
+decrypted = fernet.decrypt(encrypted)
+
+# opening the file in write mode and
+# writing the decrypted data
+with open('Zert', 'wb') as dec_file:
+    dec_file.write(decrypted)
+
+'''
+fireapp = firebase_admin.initialize_app()
+db = firestore.client()
+'''
+
+# Use a service account.
+cred = credentials.Certificate(("Zert"))
+
+fireapp = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
 
 app = FastAPI()
 
@@ -17,6 +53,40 @@ headers = {
     "accept": "application/json"
 
 }
+@app.get("/adddb")
+def read_db():
+    doc_ref = db.collection("watchlists").document(str(uuid.uuid4()))
+    doc_ref.set({
+        "name": "first list",
+        "userid": "123",
+        "movieIDS": []
+
+    })
+    return {"hello"}
+
+@app.post("/addmovie/{watchlist_id}")
+def add_movie(watchlist_id: str, movie_id: str):
+    # Get the document reference for the specified watchlist_id
+    doc_ref = db.collection("watchlists").document(watchlist_id)
+
+    # Check if the document exists
+    if not doc_ref.get().exists:
+        raise HTTPException(status_code=404, detail="Watchlist not found")
+
+    # Update the document to add the movie_id to the "movieIDS" array
+    doc_ref.update({
+        "movieIDS": firestore.ArrayUnion([movie_id])
+    })
+
+    return {"message": "Movie added successfully"}
+
+
+@app.get("/readdb")
+def read_db():
+    users_ref = db.collection("watchlists")
+    docs = users_ref.stream()
+
+    return [{doc.id: doc.to_dict()} for doc in docs]
 
 
 @app.get("/")
